2a43e083e416abe5d9bbc933fedbeca84edafdea
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-09-14 20:37:33 +0530

    Add files via upload

---
+++ OpenSource/app.py
@@ -0,0 +1,14 @@
+import os 
+from dotenv import load_dotenv
+
+from langchain_community.llms import ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+
---
+++ OpenSource/serve.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+import os
+from langserve import add_routes
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+model=ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
+
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system',system_template),
+    ('user','{text}')
+])
+
+parser = StrOutputParser()
+
+##create chain
+chain=prompt_template|model|parser
+
+##App defination
+app = FastAPI(title="langchain Server",
+              version="1.0",
+              description="a simple API server using Langchain runnable interfaces")
+
+##adding chain routes
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app,host="127.0.0.1",port=8000)
